1c35fbdfdbe3bd5ea6ce04aa1a714b30fc885ea0
Yoav Maman <yoavmmn@gmail.com>
2017-05-10 18:16:38 +0300

    refactoring get_distro

--- lib/utils.sh
+++ lib/utils.sh
@@ -8,11 +8,6 @@ not_sudo() {
 # credit to yardnsm
 get_sudo() {
 	sudo -v &> /dev/null
-	#while true; do
-	#	sudo -n true
-	#	sleep 60
-	#	kill -0 "$$" || exit
-	#done &> /dev/null &
 }
 
 # credit to yardnsm
@@ -24,44 +19,28 @@ cmd_exist() {
 get_distro() {
 # Detects which OS and if it is Linux then it will detect which Linux Distribution.
 	OS=`uname -s`
-	REV=`uname -r`
-	MACH=`uname -m`
 
-	GetVersionFromFile()
-	{
-        	VERSION=`cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // `
-	}
-
-	if [ "${OS}" = "SunOS" ] ; then
-        	OS=Solaris
-        	ARCH=`uname -p`
-        	OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
-	elif [ "${OS}" = "AIX" ] ; then
-        	OSSTR="${OS} `oslevel` (`oslevel -r`)"
-	elif [ "${OS}" = "Linux" ] ; then
+	if [ "${OS}" = "SunOS" ]; then
+		DIST='Solaris'
+	elif [ "${OS}" = "AIX" ]; then
+		DIST= 'AIX'
+	elif [ "${OS}" = "Linux" ]; then
         	KERNEL=`uname -r`
-        	if [ -f /etc/redhat-release ] ; then
+        	if [ -f /etc/redhat-release ]; then
                 	DIST='RedHat'
-                	PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-                	REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
-        	elif [ -f /etc/SuSE-release ] ; then
-                	DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
-                	REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
-        	elif [ -f /etc/mandrake-release ] ; then
+        	elif [ -f /etc/SuSE-release ]; then
+			DIST='OpenSUSE'
+        	elif [ -f /etc/mandrake-release ]; then
                 	DIST='Mandrake'
-                	PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
-                	REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
-        	elif [ -f /etc/debian_version ] ; then
+        	elif [ -f /etc/debian_version ]; then
                 	DIST="Debian"
-                	REV=""
-
         	fi
-        	if [ -f /etc/UnitedLinux-release ] ; then
+
+        	if [ -f /etc/UnitedLinux-release ]; then
                 	DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
         	fi
-
-        	OSSTR="${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
-
+	elif [ "${OS}" = "Darwin" ]; then
+		DIST='macOS'
 	fi
 
 	echo ${DIST}
