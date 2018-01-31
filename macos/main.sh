1b051b12f879d3ee91255681f63bc0b6f662f1df
yoavmmn <yoavmmn@gmail.com>
2018-01-31 22:03:41 +0200

    changed comments to be more readable

--- macos/main.sh
+++ macos/main.sh
@@ -1,10 +1,17 @@
 macos_magics() {
   echo "[*] Setting up macOS configs..."
 
-  defaults write com.apple.dock autohide -bool true # Automatically hide and show the Dock
-  defaults write com.apple.dock tilesize -integer 47 # Set the tile size to 47
-  defaults write com.apple.screencapture type jpg # change screenshot file type to jpeg
-  defaults write com.apple.Dock showhidden -bool true # changes hidden apps icon transparency
+  # Automatically hide and show the Dock
+  defaults write com.apple.dock autohide -bool true
+
+  # Set the tile size to 47
+  defaults write com.apple.dock tilesize -integer 47
+
+  # change screenshot file type to jpeg
+  defaults write com.apple.screencapture type jpg
+  
+  # changes hidden apps icon transparency
+  defaults write com.apple.Dock showhidden -bool true
 
   # compile applescripts
   path="./macos/applescripts"
