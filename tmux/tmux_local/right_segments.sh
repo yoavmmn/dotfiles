e393b796afe8aebadf4166c8e06acbd176d56e72
yoavmmn <yoavmmn@gmail.com>
2018-04-12 16:01:10 +0300

    created function for each segment, added internet connection check before updating weather status

--- tmux/tmux_local/right_segments.sh
+++ tmux/tmux_local/right_segments.sh
@@ -20,64 +20,82 @@ tm_divider() {
   echo -ne "#[fg=colour245]|#[bg=default, fg=default]"
 }
 
+_load_weather() {
+  # geo -g - get geolocation data
+  # head -3 - get only the first 3 lines
+  # tail -r - reverse lines order
+  # awk 'NR % 2 == 1' - keep only the first and third lines(city and country) the second line is region, not necessary
+  # sed 'N;s/\n/, /;' - remove new-lines and concating to a string like "Tel Aviv, Israel"
+  # weather_location// /%20 - replacing spaces to url encoded spaces
+  weather_location="$(geo -g | head -3 | tail -r | awk 'NR % 2 == 1' | sed 'N;s/\n/, /;')"
+  weather_location="${weather_location// /%20}"
+
+  # check for internet connection
+  wget -q --spider http://google.com
+
+  if [ $? -eq 0 ]; then
+    curl -s "http://wttr.in/$weather_location\?0TmQ" > "$TMP_WEATHER_FILE"
+  fi
+}
+
+
 # Weather data
-if [ ! -f ~/.tmux/weather ]; then
-  # geo -g - get geolocation data
-  # head -3 - get only the first 3 lines
-  # tail -r - reverse lines order
-  # awk 'NR % 2 == 1' - keep only the first and third lines(city and country) the second line is region, not necessary
-  # sed 'N;s/\n/, /;' - remove new-lines and concating to a string like "Tel Aviv, Israel"
-  # weather_location// /%20 - replacing spaces to url encoded spaces
-  weather_location="$(geo -g | head -3 | tail -r | awk 'NR % 2 == 1' | sed 'N;s/\n/, /;')"
-  weather_location="${weather_location// /%20}"
-  curl -s "http://wttr.in/$weather_location\?0TmQ" > ~/.tmux/weather
-fi
+weather_segment() {
+  local -r TMP_WEATHER_FILE="$HOME/.tmux/weather.tmp"
+  local -r HOT_POINT="17"
+  local -r minutes="$(date +'%M')"
+  local -r seconds="$(date +'%S')"
 
-minutes="$(date +'%M')"
-seconds="$(date +'%S')"
+  if [ ! -f "$TMP_WEATHER_FILE" ]; then
+    _load_weather
+  fi
 
-# update data every 20 minutes
-if [[ "$(($minutes % 5))" = "0" && "$seconds" -gt "00" && "$seconds" -lt "03" ]]; then
-  # geo -g - get geolocation data
-  # head -3 - get only the first 3 lines
-  # tail -r - reverse lines order
-  # awk 'NR % 2 == 1' - keep only the first and third lines(city and country) the second line is region, not necessary
-  # sed 'N;s/\n/, /;' - remove new-lines and concating to a string like "Tel Aviv, Israel"
-  # weather_location// /%20 - replacing spaces to url encoded spaces
-  weather_location="$(geo -g | head -3 | tail -r | awk 'NR % 2 == 1' | sed 'N;s/\n/, /;')"
-  weather_location="${weather_location// /%20}"
-  curl -s "http://wttr.in/$weather_location\?0TmQ" > ~/.tmux/weather
-fi
 
-weather="$(cat ~/.tmux/weather | grep -o "[0-9]* °C")"
-temprature=$(echo "$weather" | grep -o "[0-9]*")
-hot_point="17"
-weather_icon="❆"
-weather_color="blue"
+  # update data every 20 minutes
+  if [[ "$(($minutes % 5))" = "0" && "$seconds" -gt "00" && "$seconds" -lt "03" ]]; then
+    _load_weather
+  fi
 
-if [ "$temprature" -gt "$hot_point" ]; then
-  weather_icon="#[bold]☀"
-  weather_color="yellow"
-fi
+  local weather="$(cat "$TMP_WEATHER_FILE" | grep -o "[0-9]* °C")"
+  local temprature=$(echo "$weather" | grep -o "[0-9]*")
+  local weather_icon="❆"
+  local weather_color="blue"
 
-tm_segment "$weather_icon" "$weather_color" "$weather"
-tm_divider
+  if [ "$temprature" -gt "$HOT_POINT" ]; then
+    weather_icon="#[bold]☀"
+    weather_color="yellow"
+  fi
+
+  tm_segment "$weather_icon" "$weather_color" "$weather"
+  tm_divider
+}
 
 # Bettery status
-if [[ $(command -v pmset) ]]; then
-  battery_percentage="$(pmset -g batt | awk '{print $3}' | grep '%')"
-  battery_status="$(pmset -g batt | awk '{print $4}' | grep 'char')"
-  battery_color="colour28"
+battery_segment() {
+  if [[ $(command -v pmset) ]]; then
+    battery_percentage="$(pmset -g batt | awk '{print $3}' | grep '%')"
+    battery_status="$(pmset -g batt | awk '{print $4}' | grep 'char')"
+    battery_color="colour28"
 
-  [[ $battery_status == 'discharging;' ]] && battery_color="colour214"
+    [[ $battery_status == 'discharging;' ]] && battery_color="colour214"
 
-  tm_segment "" "$battery_color" "${battery_percentage%?}"
-  tm_divider
-fi
+    tm_segment "" "$battery_color" "${battery_percentage%?}"
+    tm_divider
+  fi
+}
 
 # Date and time
-tm_segment "" "colour245" "$(date +'%B %d, %H:%M')"
-tm_divider
+date_segment() {
+  tm_segment "" "colour245" "$(date +'%B %d, %H:%M')"
+  tm_divider
+}
 
 # Machine name
-tm_segment "" "blue" "#h"
+host_segment() {
+  tm_segment "" "blue" "#h"
+}
+
+weather_segment
+battery_segment
+date_segment
+host_segment
