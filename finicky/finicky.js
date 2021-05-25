75fd872028131e0fa1a52ac4be74abaa9b777459
yoavmmn <yoavmmn@gmail.com>
2021-05-26 00:57:31 +0300

    update finicky config

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -4,29 +4,55 @@ module.exports = {
     hideIcon: true,
   },
   rewrite: [
+    // force https
     {
       match: ({ url }) => url.protocol === 'http',
       url: {
         protocol: 'https',
       },
     },
+
+    // replace google with ddg
     {
-      match: ({ url }) => url.search.includes('utm_'),
+      match: finicky.matchDomains(['google.com']),
+      url: 'https://duckduckgo.com',
+    },
+
+    // remove tracking
+    {
+      match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
       url({ url }) {
+        const removeKeysStartingWith = ['utm_', 'uta_']; // Remove all query parameters beginning with these strings
+        const removeKeys = ['fblid', 'gclid']; // Remove all query parameters matching these keys
+
         const search = url.search
           .split('&')
-          .filter((part) => !part.startsWith('utm_'));
+          .map((parameter) => parameter.split('='))
+          .filter(
+            ([key]) =>
+              !removeKeysStartingWith.some((startingWith) =>
+                key.startsWith(startingWith)
+              )
+          )
+          .filter(
+            ([key]) => !removeKeys.some((removeKey) => key === removeKey)
+          );
+
         return {
           ...url,
-          search: search.join('&'),
+          search: search.map((parameter) => parameter.join('=')).join('&'),
         };
       },
     },
   ],
   handlers: [
     {
-      match: 'open.spotify.com*',
+      match: 'open.spotify.com/*',
       browser: 'Spotify',
+    },
+    {
+      match: 'notion.so*',
+      browser: 'Notion',
     },
   ],
 };
